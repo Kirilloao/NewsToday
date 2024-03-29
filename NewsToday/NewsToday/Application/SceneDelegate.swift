8af2c262a5375103329b06508816f3c99506e201
michaelbolgar <mr.mbolgar@inbox.ru>
2024-03-29 12:33:56 +0100

    cleaned code

--- NewsToday/NewsToday/Application/SceneDelegate.swift
+++ NewsToday/NewsToday/Application/SceneDelegate.swift
@@ -1,45 +1,30 @@
 import UIKit
-//
-//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-//
-//    var window: UIWindow?
-////    let factory: AppFactory = Factory()
-////
-////    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-////        guard let windowScene = (scene as? UIWindowScene) else { return }
-////        window = UIWindow(windowScene: windowScene)
-////        let rootRouter = factory.makeRootRouter(window!)
-////        rootRouter.start()
-////    }
-////    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-////        guard let _ = (scene as? UIWindowScene) else { return }
-////
-////        guard let windowScene = (scene as? UIWindowScene) else { return }
-////        let window = UIWindow(windowScene: windowScene)
-////        let startVC = SearchArticlesViewController(searchText: "test")
-////        let navigationController = UINavigationController(rootViewController: startVC)
-////        window.rootViewController = navigationController
-////        window.makeKeyAndVisible()
-////
-////        self.window = window
-////    }
-//}
-//
-
-//загрузка ViewController
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let factory: AppFactory = Factory()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
-        self.window = window
+        window = UIWindow(windowScene: windowScene)
+        let rootRouter = factory.makeRootRouter(window!)
+        rootRouter.start()
     }
+
+    //запуск экрана с поиском
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let _ = (scene as? UIWindowScene) else { return }
+//
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        let window = UIWindow(windowScene: windowScene)
+//        let startVC = SearchArticlesViewController(searchText: "test")
+//        let navigationController = UINavigationController(rootViewController: startVC)
+//        window.rootViewController = navigationController
+//        window.makeKeyAndVisible()
+//
+//        self.window = window
+//    }
 }
 
 
@@ -53,7 +38,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        guard let windowScene = (scene as? UIWindowScene) else { return }
 //
 //        let window = UIWindow(windowScene: windowScene)
-//        window.rootViewController = SearchArticlesViewController(searchText: "apple")
+//        window.rootViewController = ViewController()
 //        window.makeKeyAndVisible()
 //        self.window = window
 //    }
